45b1461e38d8da03999e0e932c9474781879804c
sandhiyamurugan <70429347+sandhiyamurugan@users.noreply.github.com>
2023-02-06 21:48:42 +0530

    Update company.sql

--- dbt_transformation_handson/models/example/company.sql
+++ dbt_transformation_handson/models/example/company.sql
@@ -6,9 +6,9 @@ with __dbt__cte__company_ab1 as (
 -- SQL model to parse JSON blob stored in a single column and extract into separated field columns as described by the JSON Schema
 -- depends_on: "my_new_database".__destination_database_._airbyte_raw_company
 select
-    jsonb_extract_path_text(_airbyte_data, 'EMP_ID') as "EMP_ID",
-    jsonb_extract_path_text(_airbyte_data, 'EMP_AGE') as EMP_AGE,
-    jsonb_extract_path_text(_airbyte_data, 'EMP_NAME') as "EMP_NAME",
+    jsonb_extract_path_text(_airbyte_data, 'ID') as "ID",
+    jsonb_extract_path_text(_airbyte_data, 'AGE') as AGE,
+    jsonb_extract_path_text(_airbyte_data, 'NAME') as "NAME",
     jsonb_extract_path_text(_airbyte_data, 'SALARY') as salary,
     jsonb_extract_path_text(_airbyte_data, 'updated_at') as updated_at,
     _airbyte_ab_id,
@@ -22,13 +22,13 @@ where 1 = 1
 -- SQL model to cast each column to its adequate SQL type converted from the JSON schema type
 -- depends_on: __dbt__cte__company_ab1
 select
-    cast("EMP_ID" as
+    cast("ID" as
     bigint
-) as "EMP_ID",
-    cast(EMP_AGE as
+) as "ID",
+    cast(AGE as
     bigint
-) as EMP_AGE,
-    cast("EMP_NAME" as text) as "EMP_NAME",
+) as AGE,
+    cast("NAME" as text) as "NAME",
     cast(salary as
     float
 ) as salary,
@@ -46,7 +46,7 @@ where 1 = 1
 -- SQL model to build a hash column based on the values of this record
 -- depends_on: __dbt__cte__company_ab2
 select
-    md5(cast(coalesce(cast("EMP_ID" as text), '') || '-' || coalesce(cast(EMP_AGE as text), '') || '-' || coalesce(cast("EMP_NAME" as text), '') || '-' || coalesce(cast(salary as text), '') || '-' || coalesce(cast(updated_at as text), '') as text)) as _airbyte_company_hashid,
+    md5(cast(coalesce(cast("ID" as text), '') || '-' || coalesce(cast(AGE as text), '') || '-' || coalesce(cast("NAME" as text), '') || '-' || coalesce(cast(salary as text), '') || '-' || coalesce(cast(updated_at as text), '') as text)) as _airbyte_company_hashid,
     tmp.*
 from __dbt__cte__company_ab2 tmp
 -- company
@@ -54,9 +54,9 @@ where 1 = 1
 )-- Final base SQL model
 -- depends_on: __dbt__cte__company_ab3
 select
-    "EMP_ID",
-    EMP_AGE,
-    "EMP_NAME",
+    "ID",
+    AGE,
+    "NAME",
     salary,
     updated_at,
     _airbyte_ab_id,
