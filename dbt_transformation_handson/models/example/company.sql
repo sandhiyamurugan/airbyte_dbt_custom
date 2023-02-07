834129fdacb669cb396d895d9ef557c15b63c5e6
sandhiyamurugan <70429347+sandhiyamurugan@users.noreply.github.com>
2023-02-07 11:39:15 +0530

    Update company.sql

--- dbt_transformation_handson/models/example/company.sql
+++ dbt_transformation_handson/models/example/company.sql
@@ -1,2 +1,14 @@
-CREATE TABLE COMPANY( ID INT PRIMARY KEY     NOT NULL, NAME           TEXT    NOT NULL, AGE            INT     NOT NULL, SALARY         REAL, updated_at
-timestamp DEFAULT NOW() NOT NULL );
+create table "my_new_database"."__destination_database_"."company__dbt_tmp"
+as (
+    select
+        _airbyte_emitted_at,
+        _airbyte_ab_id,
+        (current_timestamp at time zone 'utc')::timestamp as _airbyte_normalized_at,
+
+        cast(jsonb_extract_path_text(_airbyte_data, 'EMP_ID') as "EMP_ID",
+        cast(jsonb_extract_path_text(_airbyte_data, 'EMP_AGE') as EMP_AGE,
+        cast(jsonb_extract_path_text(_airbyte_data, 'EMP_NAME') as "EMP_NAME",
+        cast(jsonb_extract_path_text(_airbyte_data, 'SALARY') as salary,
+        cast(jsonb_extract_path_text(_airbyte_data, 'updated_at') as updated_at,
+    from "my_new_database".__destination_database_._airbyte_raw_company as table_alias
+);
